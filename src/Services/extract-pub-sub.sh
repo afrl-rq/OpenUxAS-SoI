c71d709daf212939dfecf158e97b45965e155a83
Sagar Chaki <chaki@sei.cmu.edu>
2017-05-16 23:35:59 -0400

    swicthed to version of LLVM compiled from source code.

--- src/Services/extract-pub-sub.sh
+++ src/Services/extract-pub-sub.sh
@@ -3,14 +3,14 @@
 
 function extract_sub {
     BCF=$(basename $1 .cpp).bc
-    clang++-3.9 -Wno-uninitialized -Wno-return-type -Wno-comment -std=c++11 \
-                -I../../src/Plans -I../../src/DPSS -I../../src/VisilibityLib \
-                -I../../zeromq-4.1.1/include -I../../cppzmq-4.2.1 -I../../src/LMCP \
-                -I../../src/Includes -I../../src/Communications -I../../src/Utilities \
-                -I../../3rd/PugiXML/src -emit-llvm -c \
-                -o $BCF $1
+    clang++ -Wno-uninitialized -Wno-return-type -Wno-comment -std=c++11 \
+            -I../../src/Plans -I../../src/DPSS -I../../src/VisilibityLib \
+            -I../../zeromq-4.1.1/include -I../../cppzmq-4.2.1 -I../../src/LMCP \
+            -I../../src/Includes -I../../src/Communications -I../../src/Utilities \
+            -I../../3rd/PugiXML/src -emit-llvm -c \
+            -o $BCF $1
     echo "=========== SUB $1 ==============="
-    llvm-dis-3.9 < $BCF | grep addSubscriptionAddress | awk -F'@' '{print $3}' | \
+    llvm-dis < $BCF | grep addSubscriptionAddress | awk -F'@' '{print $3}' | \
         sed 's/B5cxx11E)//g' | sed 's/^_ZN4//g' | sed 's/12Subscription$/::Subscription/g' | \
         sed -r 's/afrl[0-9]+/afrl::/g' | sed -r 's/uxas8/uxas::/g' | \
         sed -r 's/cmasi[0-9]+/cmasi::/g' | sed -r 's/impact[0-9]+/impact::/g' | \
