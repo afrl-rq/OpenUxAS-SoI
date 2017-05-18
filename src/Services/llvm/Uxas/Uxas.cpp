e050d9a0f8e11c92fde244652422accd819fc363
Sagar Chaki <chaki@sei.cmu.edu>
2017-05-17 23:13:35 -0400

    turned the pass into a module pass. other improvements.

--- src/Services/llvm/Uxas/Uxas.cpp
+++ src/Services/llvm/Uxas/Uxas.cpp
@@ -14,6 +14,7 @@
 
 #include <cxxabi.h>
 #include "llvm/ADT/Statistic.h"
+#include "llvm/IR/Module.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/Pass.h"
@@ -22,23 +23,27 @@ using namespace llvm;
 
 #define DEBUG_TYPE "uxas"
 
-STATISTIC(UxasCounter, "Counts number of functions greeted");
+//STATISTIC(UxasCounter, "Counts number of functions greeted");
 
 namespace {
   // Uxas - The first implementation, without getAnalysisUsage.
-  struct Uxas : public FunctionPass {
+  struct Uxas : public ModulePass {
     static char ID; // Pass identification, replacement for typeid
-    Uxas() : FunctionPass(ID) {}
+    Uxas() : ModulePass(ID) {}
 
-    //-- return the class name given a method of the class
+    //-- return the class name given a method of the class. if F does
+    //-- not belong to a class return an empty string as name.
     StringRef getClassName(Function &F)
     {
-      auto pt = dyn_cast<PointerType>(F.getFunctionType()->getParamType(0));
-      assert(pt);
-      auto st = dyn_cast<StructType>(pt->getElementType());
-      assert(st);
-      //-- the name always starts with "class.", so get rid of that
-      return st->getName().substr(6);
+      if(F.getFunctionType()->getNumParams()) {
+        if(auto pt = dyn_cast<PointerType>(F.getFunctionType()->getParamType(0))) {
+          if(auto *st = dyn_cast<StructType>(pt->getElementType()))
+            //-- the name always starts with "class.", so get rid of that
+            return st->getName().substr(6);
+        }
+      }
+
+      return "";
     }
 
     //-- return demangled name
@@ -49,19 +54,14 @@ namespace {
       return res.substr(0, res.find('['));
     }
 
-    bool runOnFunction(Function &F) override {
-      ++UxasCounter;
-
-      //-- ignore non-configure
-      if(!F.getName().contains("configure")) return false;
+    //-- print all messages that function F subscribes to
+    void printSubs(Function &F)
+    {
+      StringRef className = getClassName(F);
+      if(className == "") return;
       
-      //errs() << "Uxas: ";
-      //errs().write_escaped(F.getName()) << '\n';
-
       errs() << "---------------------------------------------------------------------------------\n";
-      errs() << "service : " << getClassName(F);
-      
-      errs() << " === SUBSCRIBES TO ===>\n";
+      errs() << "service : " << className << " === SUBSCRIBES TO ===>\n";
       
       //-- iterate over all basic blocks
       for(const auto &bb : F.getBasicBlockList()) {
@@ -79,10 +79,62 @@ namespace {
               errs() << '\t' << demangle(Const->getName()) << '\n';
             else if(auto *II = dyn_cast<InvokeInst>(arg)) {
               errs() << '\t' << demangle(II->getCalledFunction()->getName()) << "()\n";
-            } else assert(0);
+            }
           }          
         }
       }
+    }
+
+    //-- print all messages that function F publishes
+    void printPubs(Function &F)
+    {
+      StringRef className = getClassName(F);
+      if(className != serviceName) return;
+
+      //-- iterate over all basic blocks
+      for(const auto &bb : F.getBasicBlockList()) {
+        //-- iterate over statements
+        for(const auto &ins : bb.getInstList()) {
+          if(auto *II = dyn_cast<const InvokeInst>(&ins)) {
+            const Function *cf = II->getCalledFunction();
+            if(cf == NULL || !cf->getName().contains("sendSharedLmcpObjectBroadcastMessage")) continue;   
+
+            //-- the first argument to a method call is always the
+            //-- this pointer. so get the second argument.
+            auto *arg = II->getArgOperand(1);
+            auto *type = arg->getType();
+            assert(type);
+            errs() << *type << '\n';
+          }          
+        }
+      }
+    }
+
+    //-- name of the service class being analyzed
+    StringRef serviceName;
+
+    //-- the top level method
+    bool runOnModule(Module &M) override
+    {
+      //-- first get the name of the service class by looking for the
+      //-- configure method and seeing which class it belongs to
+      for(Function &F : M.getFunctionList()) {
+        if(!F.getName().contains("configure")) continue;
+        StringRef className = getClassName(F);
+        if(className == "") continue;
+        serviceName = className;
+        errs() << "got service name " << serviceName << '\n';
+        printSubs(F);
+        break;
+      }
+      
+      errs() << "---------------------------------------------------------------------------------\n";
+      errs() << "service : " << serviceName << " === PUBLISHES TO ===>\n";
+      
+      //-- now extract the pub-sub information
+      for(Function &F : M.getFunctionList()) {
+        if(!F.getName().contains("configure")) printPubs(F);
+      }      
       
       return false;
     }
