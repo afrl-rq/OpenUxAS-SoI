8113f904d2a6ab7f9b0ffa8c0c1f929104988199
derek.kingston <derek.kingston@us.af.mil>
2017-06-13 09:17:39 -0400

    Use Factory class for packing messages

--- src/Services/Waypoint2Sel4Service.cpp
+++ src/Services/Waypoint2Sel4Service.cpp
@@ -17,6 +17,8 @@
 
 
 #include "Waypoint2Sel4Service.h"
+#include "avtas/lmcp/ByteBuffer.h"
+#include "avtas/lmcp/Factory.h"
 #include "afrl/cmasi/AutomationResponse.h"
 #include "afrl/cmasi/MissionCommand.h"
 
@@ -34,7 +36,6 @@
 
 #include <iostream>
 
-#define STRING_XML_TYPE "Type"
 #define STRING_XML_VEHICLE_ID "VehicleID"
 
 #define COUT_INFO(MESSAGE) std::cout << "<>Waypoint2Sel4Service:: " << MESSAGE << std::endl;std::cout.flush();
@@ -57,16 +58,7 @@ Waypoint2Sel4Service::~Waypoint2Sel4Service() { };
 bool
 Waypoint2Sel4Service::configure(const pugi::xml_node& ndComponent)
 {
-    std::string strBasePath = m_workDirectoryPath;
-    uint32_t ui32EntityID = m_entityId;
-    uint32_t ui32LmcpMessageSize_max = 100000;
-    std::stringstream sstrErrors;
-
-    bool bSucceeded(true);
-
     m_vehicleID = m_entityId; // can be overridden below
-
-    std::string strComponentType = ndComponent.attribute(STRING_XML_TYPE).value();
 
     if (!ndComponent.attribute(STRING_XML_VEHICLE_ID).empty())
     {
@@ -75,7 +67,7 @@ Waypoint2Sel4Service::configure(const pugi::xml_node& ndComponent)
 
     addSubscriptionAddress(afrl::cmasi::AutomationResponse::Subscription);
     addSubscriptionAddress(afrl::cmasi::MissionCommand::Subscription); 
-    return (bSucceeded);
+    return true;
 }
 
 bool Waypoint2Sel4Service::initialize()
@@ -104,34 +96,33 @@ void Waypoint2Sel4Service::write2Sel4(afrl::cmasi::MissionCommand * mission){
 
     if (mission->getVehicleID() == m_vehicleID)
     {
-        avtas::lmcp::ByteBuffer buf;
         mission->getVehicleActionList().clear();
         auto waypointList = mission->getWaypointList();
         for(auto waypoint : waypointList){
             waypoint->getVehicleActionList().clear();
             waypoint->getAssociatedTasks().clear();
         }
-        buf.allocate(mission->calculatePackedSize());
-        mission->pack(buf);
+        avtas::lmcp::ByteBuffer* buf = avtas::lmcp::Factory::packMessage(mission, true);
         #if 1 == 1
         int fd = open("/dev/mem", O_RDWR | O_SYNC);
         uint8_t * mem = (uint8_t *)mmap(0, 1, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0xE0000000);
 
 	COUT_INFO(mission->toString());
         COUT_INFO("Capacity: ");
-        COUT_INFO(buf.capacity());
-	pArray = buf.array();
-	for(i = 0; i < buf.capacity(); i++){
+        COUT_INFO(buf->capacity());
+	pArray = buf->array();
+	for(i = 0; i < buf->capacity(); i++){
             *mem = *(pArray++);
 	}
-//        while(buf.hasRemaining()){
-//            *mem = buf.get();
+//        buf->rewind();
+//        while(buf->hasRemaining()){
+//            *mem = buf->get();
 //        }
 
         close(fd);
         #else
         FILE * f = fopen("/tmp/missioncommands", "wb");
-        fwrite(buf.array(),1,buf.capacity(),f);
+        fwrite(buf->array(),1,buf->capacity(),f);
         fclose(f);
         #endif
     }
@@ -152,7 +143,6 @@ Waypoint2Sel4Service::processReceivedLmcpMessage(std::unique_ptr<uxas::communica
         }
     }
     else if (afrl::cmasi::isMissionCommand(receivedLmcpMessage->m_object))
-    //else if (receivedLmcpMessage->m_object->getLmcpTypeName() == "MissionCommand")
     {
         auto mission = std::static_pointer_cast<afrl::cmasi::MissionCommand> (receivedLmcpMessage->m_object);
 	COUT_INFO("Writing message!\n");
