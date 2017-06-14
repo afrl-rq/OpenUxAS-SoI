f777317e85630ffc5ff47bf9a30a6eaf04d3f101
Sagar Chaki <chaki@sei.cmu.edu>
2017-06-14 16:15:52 -0400

    updated script to save output in files.

--- examples/11_GamsServiceDriver_SharedMemory/runUxAS.sh
+++ examples/11_GamsServiceDriver_SharedMemory/runUxAS.sh
@@ -5,7 +5,7 @@ SAVE_DIR=$(pwd)
 RM_DATAWORK="rm -fR ./datawork"
 RM_LOG="rm -fR ./log"
 
-BIN="$UXAS_ROOT/build_debug/uxas"
+BIN="$UXAS_ROOT/build/uxas"
 
 function run_uxas {
     EID="$1"
@@ -14,9 +14,9 @@ function run_uxas {
     [ ! -e checkpoints ] && mkdir checkpoints
     $RM_DATAWORK
     $RM_LOG
-    $BIN -cfgPath ../cfg_GamsServiceDriver_SharedMemory_$EID.xml &> out.$EID
+    $BIN -cfgPath ../cfg_GamsServiceDriver_SharedMemory_$EID.xml
 }
 
-run_uxas 400 &
-run_uxas 500 &
+run_uxas 400 &> /tmp/out.400 &
+run_uxas 500 2>&1 | tee /tmp/out.500
 wait
