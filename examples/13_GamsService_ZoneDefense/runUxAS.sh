944dddf18ff579d79ec0e525846fbf841df0aff2
James Edmondson <jedmondson@gmail.com>
2017-06-22 13:25:55 -0400

    * Adding scaffolding for ZoneDefense example

---
+++ examples/13_GamsService_ZoneDefense/runUxAS.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+SAVE_DIR=$(pwd)
+
+RM_DATAWORK="rm -fR ./datawork"
+RM_LOG="rm -fR ./log"
+
+BIN="$UXAS_ROOT/build/uxas"
+
+function run_uxas {
+    EID="$1"
+    [ ! -e RUNDIR_$EID ] && mkdir -p RUNDIR_$EID
+    cd RUNDIR_$EID
+    [ ! -e checkpoints ] && mkdir checkpoints
+    $RM_DATAWORK
+    $RM_LOG
+    $BIN -cfgPath ../cfg_$EID.xml
+}
+
+run_uxas 400 &> /tmp/out.400 &
+run_uxas 500 2>&1 | tee /tmp/out.500
+run_uxas 600 2>&1 | tee /tmp/out.600
+run_uxas 700 2>&1 | tee /tmp/out.700
+run_uxas 800 2>&1 | tee /tmp/out.800
+wait
