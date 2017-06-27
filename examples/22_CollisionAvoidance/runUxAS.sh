77b92b2096b515ebbc3127469af9b650204b6fa5
Sagar Chaki <chaki@sei.cmu.edu>
2017-06-27 08:00:02 -0400

    initial commit of example 22. this is just a copy of example 20.

---
+++ examples/22_CollisionAvoidance/runUxAS.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+SAVE_DIR=$(pwd)
+
+RM_DATAWORK="rm -fR ./datawork"
+RM_LOG="rm -fR ./log"
+
+BIN="$UXAS_ROOT/build/uxas"
+
+function run_uxas {
+    EID="$1"
+    [ ! -e RUNDIR_$EID ] && mkdir -p RUNDIR_$EID
+    cd RUNDIR_$EID
+    [ ! -e checkpoints ] && mkdir checkpoints
+    $RM_DATAWORK
+    $RM_LOG
+    $BIN -cfgPath ../cfg_$EID.xml
+}
+
+run_uxas 400 &> /tmp/out.400 &
+run_uxas 500 2>&1 | tee /tmp/out.500
+wait
