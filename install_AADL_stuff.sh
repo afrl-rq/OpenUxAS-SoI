ddd970e06493d0cce690c9af2a03f9926c7a8db7
Catharine McGhan <cat.mcghan@uc.edu>
2017-06-30 11:44:14 -0400

    updating install_AADL_stuff.sh to newest Tool_Installation_and_Setup_Instructions.docx to account for requires updates to OSATE2 plugins (AGREE, Resolute); also reconfigured the if-elif-fi statements to be a bit more in-line with each other

--- install_AADL_stuff.sh
+++ install_AADL_stuff.sh
@@ -5,7 +5,7 @@
 # https://github.com/afrl-rq/OpenUxAS
 # Additional copyright may be held by others, as reflected in the commit history.
 
-# from Tool_Installation_and_Setup_Instructions.docx, 2017-05-24:
+# from Tool_Installation_and_Setup_Instructions.docx, 2017-06-30:
 # For the most-updated instructions, see:
 # -- https://github.com/afrl-rq/OpenUxAS/tree/architecture/AADL_project/doc
 
@@ -39,9 +39,6 @@ fi
 
 if [ "$(uname)" == "Darwin" ]; then
 #if [ "$($(uname -s) | cut -c 1-6)" == "Darwin" ]; then
-
-    mkdir -p ~/osate2-2.2.2
-
     echo "Install Prerequisites on Mac OS X"
     echo " "
     # Install homebrew (must be administrator): in terminal
@@ -54,71 +51,10 @@ if [ "$(uname)" == "Darwin" ]; then
     # unzip-to-7za: https://superuser.com/questions/626721/trying-to-unzip-file
     #brew install p7zip # call via 7za x file.zip
     #brew install gnu-sed # call by gsed
-    # Install Oracle Java run-time (required for LmcpGen)
-    # grab the .dmg file from: https://java.com/en/download/manual.jsp
-    wget -O jre-XXXXX-macosx-x64.dmg http://javadl.oracle.com/webapps/download/AutoDL?BundleId=220306_d54c1d3a095b4ff2b6607d096fa80163
-    # as of 2017-05-08, this is: jre-8u131-macosx-x64.dmg
-    # command modified from: http://stackoverflow.com/questions/22934083/install-dmg-package-on-mac-os-from-terminal
-    MOUNTDIR=$(echo `hdiutil mount jre-XXXXX-macosx-x64.dmg | tail -1 | awk '{$1=$2=""; print $0}'` | xargs -0 echo) && sudo installer -pkg "${MOUNTDIR}/"*.pkg -target / 
-    echo "Installing OSATE 2.2.2..."
-    cd ~/osate2-2.2.2
-    wget -O osate2-2.2.2-vfinal-linux.gtk.x86_64.tar.gz http://aadl.info/aadl/osate/stable/2.2.2/products/osate2-2.2.2-vfinal-linux.gtk.x86_64.tar.gz
-    tar -xvzf osate2-2.2.2-vfinal-linux.gtk.x86_64.tar.gz
-    echo "Running OSATE2, v.2.2.2..."
-    echo "* Choose a workspace, then press 'OK'."
-    echo "Once you're done..."
-    echo "Press any key to continue..." # reference: https://ss64.com/bash/read.html
-    ./osate &
-    read -rs -p " " -n 1 # reference: https://ss64.com/bash/read.html
-    echo " "
-    echo "Install the Z3 Plugin inside OSATE2 (used by AGREE)"
-    echo "* Select 'Help'->'Install New Software...'"
-    echo "* In the 'Work with:' field, type the following, then hit enter:"
-    echo "https://raw.githubusercontent.com/smaccm/update-site/master/site.xml"
-    echo "Once you're done..."
-    echo "Press any key to continue..." # reference: https://ss64.com/bash/read.html
-    read -rs -p " " -n 1 # reference: https://ss64.com/bash/read.html
-    echo " "
-    echo "Check tool versions used / plugin installation..."
-    echo "* Select 'Help'->'Installation Details'"
-    echo "Once you're done..."
-    echo "Press any key to continue..." # reference: https://ss64.com/bash/read.html
-    read -rs -p " " -n 1 # reference: https://ss64.com/bash/read.html
-    echo " "
-    echo "Set OSATE Preferences for AGREE..."
-    echo "* Select 'Window'->'Preferences', then 'Agree'->'Analysis'"
-    echo "* In the Analysis panel, under 'Agree Analysis Settings':"
-    echo "  * 'Model Checker Setting' should be JKind"
-    echo "  * 'SMT Solver' should be Z3"
-    echo "  * Set 'Maximum Number of PDR Instances' to '2' if 4+ CPU cores, '1' otherwise"
-    echo "  * Uncheck 'Generate inductive counterexamples'"
-    echo "  * Click 'Apply' to save settings"
-    echo "* Click 'OK' to exit Preferences window"
-    echo "Once you're done..."
-    echo "Press any key to continue..." # reference: https://ss64.com/bash/read.html
-    read -rs -p " " -n 1 # reference: https://ss64.com/bash/read.html
-    echo " "
-    echo "Import the UxAS AADL Project..."
-    echo "* Select 'File'->'Import', then 'General'->'Existing Projects into Workspace'"
-    echo "* Click 'Next'"
-    echo "* In the Import popup window:"
-    echo "  * 'Select root directory:' should be the ./OpenUxAS/AADL_project directory, so browse"
-    echo "    to the directory and then click 'OK'"
-    echo "    (e.g., if you used the default OpenUxAS workspace directory in ./checkout_plus_config.sh,"
-    echo "     then in another window you're going to need to swap branches first, via:"
-    echo "     cd /home/$USER/UxAS_pulls/OpenUxAS && git checkout architecture"
-    echo "     and then the project's root directory will be: /home/$USER/UxAS_pulls/OpenUxAS/AADL_project )"
-    echo "  * Under 'Projects:', make sure that the 'UxAS' box is checked"
-    echo "  * Click 'Finish' to exit the Import window"
-    echo "Once you're done..."
-    echo "Press any key to continue..." # reference: https://ss64.com/bash/read.html
-    read -rs -p " " -n 1 # reference: https://ss64.com/bash/read.html
-    echo " "
     
-    echo "...Congratulations! You're done with the installation and setup!"
-
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-
+    echo "Installing Prerequisite Tools on Ubuntu Linux (/ ...Bash on Ubuntu on Windows?)"
+    echo "First -- update Ubuntu Linux packages if needed!"
     # run an 'apt update' check without sudo
     # ref: https://askubuntu.com/questions/391983/software-updates-from-terminal-without-sudo
     aptdcon --refresh
@@ -131,17 +67,36 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
         echo "Done with apt update-upgrade!"
     fi
     sudo apt -y install firefox wget tar unzip sed
-    
-    mkdir -p /home/$USER/osate2-2.2.2
+fi
+echo " "
 
-    echo "Installing Prerequisite Tools on Ubuntu Linux (/ ...Bash on Ubuntu on Windows?)"
+echo "Install Oracle Java run-time (required for LmcpGen)"
+if [ "$(uname)" == "Darwin" ]; then
+#if [ "$($(uname -s) | cut -c 1-6)" == "Darwin" ]; then
+    # Install Oracle Java run-time (required for LmcpGen)
+    # grab the .dmg file from: https://java.com/en/download/manual.jsp
+    wget -O jre-XXXXX-macosx-x64.dmg http://javadl.oracle.com/webapps/download/AutoDL?BundleId=220306_d54c1d3a095b4ff2b6607d096fa80163
+    # as of 2017-05-08, this is: jre-8u131-macosx-x64.dmg
+    # command modified from: http://stackoverflow.com/questions/22934083/install-dmg-package-on-mac-os-from-terminal
+    MOUNTDIR=$(echo `hdiutil mount jre-XXXXX-macosx-x64.dmg | tail -1 | awk '{$1=$2=""; print $0}'` | xargs -0 echo) && sudo installer -pkg "${MOUNTDIR}/"*.pkg -target / 
+
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # Install Oracle Java run-time (required for LmcpGen): in terminal
-    echo " "
     sudo add-apt-repository ppa:webupd8team/java
     sudo apt update; sudo apt -y install oracle-java8-installer
     sudo apt -y install oracle-java8-set-default
+fi
+
+echo "Installing OSATE2 2.2.2..."
+if [ "$(uname)" == "Darwin" ]; then
+#if [ "$($(uname -s) | cut -c 1-6)" == "Darwin" ]; then
+    mkdir -p ~/osate2-2.2.2
+    cd ~/osate2-2.2.2
+    wget -O osate2-2.2.2-vfinal-linux.gtk.x86_64.tar.gz http://aadl.info/aadl/osate/stable/2.2.2/products/osate2-2.2.2-vfinal-linux.gtk.x86_64.tar.gz
+    tar -xvzf osate2-2.2.2-vfinal-linux.gtk.x86_64.tar.gz
     
-    echo "Installing OSATE2 2.2.2..."
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    mkdir -p /home/$USER/osate2-2.2.2
     cd /home/$USER/osate2-2.2.2
     wget -O osate2-2.2.2-vfinal-linux.gtk.x86_64.tar.gz http://aadl.info/aadl/osate/stable/2.2.2/products/osate2-2.2.2-vfinal-linux.gtk.x86_64.tar.gz
     tar -xvzf osate2-2.2.2-vfinal-linux.gtk.x86_64.tar.gz
@@ -156,60 +111,99 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
         #line 5: --launcher.library
         sed -i.orig '3s/--launcher.library/--launcher.GTK_version\n2\n--launcher.library/' osate.ini
     fi
-    echo "Running OSATE2, v.2.2.2..."
-    echo "* Choose a workspace, check 'Use this as the default and do not ask again', then press 'OK'."
-    echo "Once you're done..."
-    echo "Press any key to continue..." # reference: https://ss64.com/bash/read.html
-    ./osate &
-    read -rs -p " " -n 1 # reference: https://ss64.com/bash/read.html
-    echo " "
-    echo "Install the Z3 Plugin inside OSATE2 (used by AGREE)"
-    echo "* Select 'Help'->'Install New Software...'"
-    echo "* In the 'Work with:' field, type the following, then hit enter:"
-    echo "https://raw.githubusercontent.com/smaccm/update-site/master/site.xml"
-    echo "* Wait for the list to load, then open out 'SMACCM' and check the 'Z3 Plugin' box"
-    echo "* Click 'Next', click 'Next' to confirm, accept the license agreement terms, click 'Finish'"
-    echo "* In the 'Security Warning' dialogue box, click 'OK'"
-    echo "* Click 'Yes' to restart OSATE2"
-    echo "Once you're done..."
-    echo "Press any key to continue..." # reference: https://ss64.com/bash/read.html
-    read -rs -p " " -n 1 # reference: https://ss64.com/bash/read.html
-    echo " "
-    echo "Check tool versions used / plugin installation..."
-    echo "* Select 'Help'->'Installation Details'"
-    echo "Once you're done..."
-    echo "Press any key to continue..." # reference: https://ss64.com/bash/read.html
-    read -rs -p " " -n 1 # reference: https://ss64.com/bash/read.html
-    echo " "
-    echo "Set OSATE Preferences for AGREE..."
-    echo "* Select 'Window'->'Preferences', then 'Agree'->'Analysis'"
-    echo "* In the Analysis panel, under 'Agree Analysis Settings':"
-    echo "  * 'Model Checker Setting' should be JKind"
-    echo "  * 'SMT Solver' should be Z3"
-    echo "  * Set 'Maximum Number of PDR Instances' to '2' if 4+ CPU cores, '1' otherwise"
-    echo "  * Uncheck 'Generate inductive counterexamples'"
-    echo "  * Click 'Apply' to save settings"
-    echo "* Click 'OK' to exit Preferences window"
-    echo "Once you're done..."
-    echo "Press any key to continue..." # reference: https://ss64.com/bash/read.html
-    read -rs -p " " -n 1 # reference: https://ss64.com/bash/read.html
-    echo " "
-    echo "Import the UxAS AADL Project..."
-    echo "* Select 'File'->'Import', then 'General'->'Existing Projects into Workspace'"
-    echo "* Click 'Next'"
-    echo "* In the Import popup window:"
-    echo "  * 'Select root directory:' should be the ./OpenUxAS/AADL_project directory, so browse"
-    echo "    to the directory and then click 'OK'"
-    echo "    (e.g., if you used the default OpenUxAS workspace directory in ./checkout_plus_config.sh,"
-    echo "     then in another window you're going to need to swap branches first, via:"
-    echo "     cd /home/$USER/UxAS_pulls/OpenUxAS && git checkout architecture"
-    echo "     and then the project's root directory will be: /home/$USER/UxAS_pulls/OpenUxAS/AADL_project )"
-    echo "  * Under 'Projects:', make sure that the 'UxAS' box is checked"
-    echo "  * Click 'Finish' to exit the Import window"
-    echo "Once you're done..."
-    echo "Press any key to continue..." # reference: https://ss64.com/bash/read.html
-    read -rs -p " " -n 1 # reference: https://ss64.com/bash/read.html
-    echo " "
+#elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then # Win32
+#    mkdir C:\AADL_stuff
+#    wget http://aadl.info/aadl/osate/stable/2.2.2/products/osate2-2.2.2-vfinal-win32.win32.x86.zip
+#    unzip osate2-2.2.2-vfinal-win32.win32.x86.zip
+#elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then # Win64
+#    mkdir C:\AADL_stuff
+#    wget http://aadl.info/aadl/osate/stable/2.2.2/products/osate2-2.2.2-vfinal-win32.win32.x86_64.zip
+#    unzip osate2-2.2.2-vfinal-win32.win32.x86_64.zip    
+fi
+
+# same for both platforms ("safe" because exited out earlier if not MacOSX or Linux already):
+echo "Running OSATE2, v.2.2.2..."
+echo "* Choose a workspace, then press 'OK'."
+echo "Once you're done..."
+echo "Press any key to continue..." # reference: https://ss64.com/bash/read.html
+./osate &
+read -rs -p " " -n 1 # reference: https://ss64.com/bash/read.html
+echo " "
+
+# same for both platforms ("safe" because exited out earlier if not MacOSX or Linux already):
+echo "Uninstall the AGREE and Resolute Plugins inside OSATE2 (prerequisite for installing updated plugins)"
+echo "* Select 'Help'->'Installation Details'"
+echo "* In the 'Installed Software' tab, select 'Agree' and 'Resolute' from the list of installed software"
+echo "  (for multi-select: left-click on 'Agree', hold down Ctrl key, left-click on 'Resolute', let go of Ctrl key)"
+echo "* Click 'Uninstall...'"
+echo "* In the 'Uninstall' dialogue box, click 'Finish'"
+echo "* Click 'Yes' to restart OSATE2"
+echo "Once you're done..."
+echo "Press any key to continue..." # reference: https://ss64.com/bash/read.html
+read -rs -p " " -n 1 # reference: https://ss64.com/bash/read.html
+echo " "
+
+# same for both platforms ("safe" because exited out earlier if not MacOSX or Linux already):
+echo "Install the (updated) AGREE and Resolute Plugins, and the Z3 Plugin (used by AGREE), inside OSATE2"
+echo "* Select 'Help'->'Install New Software...'"
+echo "* In the 'Work with:' field, type the following, then hit enter:"
+echo "https://raw.githubusercontent.com/smaccm/update-site/master/site.xml"
+echo "* Wait for the list to load, then open out 'SMACCM' and check the 'Agree', 'Resolute', and 'Z3 Plugin' boxes"
+echo "* Click 'Next', click 'Next' to confirm, accept the license agreement terms, click 'Finish'"
+echo "* In the 'Security Warning' dialogue box, click 'OK'"
+echo "* Click 'Yes' to restart OSATE2"
+echo "Once you're done..."
+echo "Press any key to continue..." # reference: https://ss64.com/bash/read.html
+read -rs -p " " -n 1 # reference: https://ss64.com/bash/read.html
+echo " "
+
+# same for both platforms ("safe" because exited out earlier if not MacOSX or Linux already):
+echo "Check tool versions used / plugin installation..."
+echo "* Select 'Help'->'Installation Details'"
+echo "Once you're done..."
+echo "Press any key to continue..." # reference: https://ss64.com/bash/read.html
+read -rs -p " " -n 1 # reference: https://ss64.com/bash/read.html
+echo " "
+
+# same for both platforms ("safe" because exited out earlier if not MacOSX or Linux already):
+echo "Set OSATE Preferences for AGREE..."
+echo "* Select 'Window'->'Preferences', then 'Agree'->'Analysis'"
+echo "* In the Analysis panel, under 'Agree Analysis Settings':"
+echo "  * 'Model Checker Setting' should be JKind"
+echo "  * 'SMT Solver' should be Z3"
+echo "  * Set 'Maximum Number of PDR Instances' to '2' if 4+ CPU cores, '1' otherwise"
+echo "  * Uncheck 'Generate inductive counterexamples'"
+echo "  * Click 'Apply' to save settings"
+echo "* Click 'OK' to exit Preferences window"
+echo "Once you're done..."
+echo "Press any key to continue..." # reference: https://ss64.com/bash/read.html
+read -rs -p " " -n 1 # reference: https://ss64.com/bash/read.html
+echo " "
+
+# same for both platforms ("safe" because exited out earlier if not MacOSX or Linux already):
+echo "Import the UxAS AADL Project..."
+echo "* Select 'File'->'Import', then 'General'->'Existing Projects into Workspace'"
+echo "* Click 'Next'"
+echo "* In the Import popup window:"
+echo "  * 'Select root directory:' should be the ./OpenUxAS/AADL_project directory, so browse"
+echo "    to the directory and then click 'OK'"
+echo "    (e.g., if you used the default OpenUxAS workspace directory in ./checkout_plus_config.sh,"
+echo "     then in another window you're going to need to swap branches first, via:"
+echo "     cd /home/$USER/UxAS_pulls/OpenUxAS && git checkout architecture"
+echo "     and then the project's root directory will be: /home/$USER/UxAS_pulls/OpenUxAS/AADL_project )"
+echo "  * Under 'Projects:', make sure that the 'UxAS' box is checked"
+echo "  * Click 'Finish' to exit the Import window"
+echo "Once you're done..."
+echo "Press any key to continue..." # reference: https://ss64.com/bash/read.html
+read -rs -p " " -n 1 # reference: https://ss64.com/bash/read.html
+echo " "
+
+if [ "$(uname)" == "Darwin" ]; then
+#if [ "$($(uname -s) | cut -c 1-6)" == "Darwin" ]; then
+    : # pass
+    
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    : # pass
     
     #
     # installing Z3 from source:
@@ -237,22 +231,10 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # and: https://github.com/smaccm/update-site/blob/master/site.xml
     # which led to: http://aadl.info/aadl/osate/testing/update-site/
     # alternately: http://aadl.info/aadl/osate/stable/2.2.2/update-site/
-    
-    echo "...Congratulations! You're done with the installation and setup!"
-
-#elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then # Win32
-    
-#    mkdir C:\AADL_stuff
-#    wget http://aadl.info/aadl/osate/stable/2.2.2/products/osate2-2.2.2-vfinal-win32.win32.x86.zip
-#    unzip osate2-2.2.2-vfinal-win32.win32.x86.zip
-    
-#elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then # Win64
-
-#    mkdir C:\AADL_stuff
-#    wget http://aadl.info/aadl/osate/stable/2.2.2/products/osate2-2.2.2-vfinal-win32.win32.x86_64.zip
-#    unzip osate2-2.2.2-vfinal-win32.win32.x86_64.zip
-    
 fi
+
+echo "...Congratulations! You're done with the installation and setup!"
+
 
 echo "All done with script 'install_AADL_stuff.sh'!"
 
