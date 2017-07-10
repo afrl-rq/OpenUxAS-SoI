8d23b9854950771961c29f6f8c831f0e78df0f0f
John Backes <john.backes@gmail.com>
2017-07-10 10:36:00 -0400

    making stuff build after adding entry points

--- sel4_artifacts/UxASSeL4/components/Asset_Waypoint_Manager/src/asset_waypoint_manager.c
+++ sel4_artifacts/UxASSeL4/components/Asset_Waypoint_Manager/src/asset_waypoint_manager.c
@@ -1,18 +1,26 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <tb_Asset_Waypoint_Manager.h>
 
-void component_entry(void) {
-
-}
-
-void component_init(void) {
+void component_entry(const int64_t * periodic_dispatcher){
 
 }
 
-void mission_read_vm(bool * unused) {
+void component_init(const int64_t *arg){
+
+}
+
+void mission_read_vm(const bool * tb_waypoint_read_vm) {
 	printf("%i:%s:%s",__LINE__,__FILE__,__FUNCTION__);
 }
 
-void mission_read_wm(bool * unused) {
+
+void mission_read_wm(const bool * tb_waypoint_read_wm) {
 	printf("%i:%s:%s",__LINE__,__FILE__,__FUNCTION__);
+}
+
+
+void in_uart_packet(const SMACCM_DATA__UART_Packet_i * tb_in_uart_packet){
+
+
 }
