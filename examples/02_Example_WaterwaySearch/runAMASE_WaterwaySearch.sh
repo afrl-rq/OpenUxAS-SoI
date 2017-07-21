a2e81cfc30cdca6e2dd2e7afca032e6c33b8f236
Erkan <etuncali@asu.edu>
2017-07-21 14:41:55 -0400

    Waterway example running script is corrected.

--- examples/02_Example_WaterwaySearch/runAMASE_WaterwaySearch.sh
+++ examples/02_Example_WaterwaySearch/runAMASE_WaterwaySearch.sh
@@ -1,6 +1,6 @@
 here=$PWD;
 
 cd ../../../OpenAMASE/OpenAMASE;
-java -Xmx2048m -classpath ./dist/*:./lib/*  avtas.app.Application --config config/amase_headless --scenario "../../OpenUxAS/examples/02_Example_WaterwaySearch/Scenario_WaterwaySearch.xml";
+java -Xmx2048m -classpath ./dist/*:./lib/*  avtas.app.Application --config config/amase --scenario "../../OpenUxAS/examples/02_Example_WaterwaySearch/Scenario_WaterwaySearch.xml";
 cd "$here";
 
