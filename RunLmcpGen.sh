e51faaf6cd545dae7405f58d0f790f581e8aa9a3
derekkingston <derek.kingston@gmail.com>
2017-11-30 22:12:33 -0500

    Allow RunLmcpGen to auto-re-build AMASE with compatible LMCP lib

--- RunLmcpGen.sh
+++ RunLmcpGen.sh
@@ -1,8 +1,19 @@
 #! /bin/bash
 
+# use LmcpGen via GUI
 #java -Xmx2048m -jar ../LmcpGen/dist/LmcpGen.jar
+
+# auto-create documentation, c++, and python libraries
 java -Xmx2048m -jar ../LmcpGen/dist/LmcpGen.jar -mdmdir "mdms" -cpp -dir "src/LMCP"
-java -Xmx2048m -jar ../LmcpGen/dist/LmcpGen.jar -mdmdir "mdms" -java -dir "../OpenAMASE/OpenAMASE/lib/LMCP"
 java -Xmx2048m -jar ../LmcpGen/dist/LmcpGen.jar -mdmdir "mdms" -doc -dir "doc/LMCP"
 java -Xmx2048m -jar ../LmcpGen/dist/LmcpGen.jar -mdmdir "mdms" -py -dir "src/LMCP/py"
 
+# build and install java library for AMASE
+java -Xmx2048m -jar ../LmcpGen/dist/LmcpGen.jar -mdmdir "mdms" -java -dir "../OpenAMASE/OpenAMASE/lib/LMCP"
+cd ../OpenAMASE/OpenAMASE/lib/LMCP
+ant -q jar
+cd dist
+cp lmcplib.jar ../..
+cd ../../..
+ant -q jar
+cd ../../OpenUxAS
