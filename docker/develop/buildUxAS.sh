4ccb63a856626fcbe4134dec2b32461ca08164fc
steve <steve@steve-16.04>
2017-12-14 17:05:16 -0500

    added docker files for building and deploying
    Former-commit-id: e7b8007c9309a85d021272dde7177474c26fced0

---
+++ docker/develop/buildUxAS.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+# 1 - change to the directory: OpenUxAS
+cd /OpenUxAS
+
+# if builds exists the just run Ninja
+if [ ! -d "build" ]; then
+	meson build --buildtype=release
+fi
+
+# 3 - compile the code
+ninja -C build all
