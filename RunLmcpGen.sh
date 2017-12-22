390f4df7a200d4c5745ed2ddc901eec210914c7c
steve <steve@steve-16.04>
2017-12-22 09:43:56 -0500

    checks for lmcp directory and builds lmcpgen everytime
    Former-commit-id: a8861c81e0cba1207bac8ec1f658fa91d8b1ee5c

--- RunLmcpGen.sh
+++ RunLmcpGen.sh
@@ -1,8 +1,22 @@
-#! /bin/bash
+#! /bin/bash -e
 
-#java -Xmx2048m -jar ../LmcpGen/dist/LmcpGen.jar
-java -Xmx2048m -jar ../LmcpGen/dist/LmcpGen.jar -mdmdir "mdms" -cpp -dir "src/LMCP"
-java -Xmx2048m -jar ../LmcpGen/dist/LmcpGen.jar -mdmdir "mdms" -java -dir "../OpenAMASE/OpenAMASE/lib/LMCP"
-java -Xmx2048m -jar ../LmcpGen/dist/LmcpGen.jar -mdmdir "mdms" -doc -dir "doc/LMCP"
-java -Xmx2048m -jar ../LmcpGen/dist/LmcpGen.jar -mdmdir "mdms" -py -dir "src/LMCP/py"
+HERE=$PWD;
 
+DIRECTORY="../LmcpGen"
+
+if [ -d "${DIRECTORY}" ]; then
+
+	# build lmcpgen
+	cd ${DIRECTORY}
+	ant -q jar
+	cd ${HERE}
+
+	#java -Xmx2048m -jar ../LmcpGen/dist/LmcpGen.jar
+	java -Xmx2048m -jar ../LmcpGen/dist/LmcpGen.jar -mdmdir "mdms" -cpp -dir "src/LMCP"
+	java -Xmx2048m -jar ../LmcpGen/dist/LmcpGen.jar -mdmdir "mdms" -java -dir "../OpenAMASE/OpenAMASE/lib/LMCP"
+	java -Xmx2048m -jar ../LmcpGen/dist/LmcpGen.jar -mdmdir "mdms" -doc -dir "doc/LMCP"
+	java -Xmx2048m -jar ../LmcpGen/dist/LmcpGen.jar -mdmdir "mdms" -py -dir "src/LMCP/py"
+else
+	echo "ERROR: LmcpGen must be present!!!"
+	exit 1
+fi
