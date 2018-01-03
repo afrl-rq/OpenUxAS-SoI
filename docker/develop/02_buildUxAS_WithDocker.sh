f624e470ec0cb6696e0548f7490a445069c623a1
steve <steve@steve-16.04>
2018-01-03 16:40:06 -0500

    - fixed docker to work with internal code, updated readme's
    Former-commit-id: 0d1d35d409d0b7f9e180cd974492bf474e2ff128

--- docker/develop/02_buildUxAS_WithDocker.sh
+++ docker/develop/02_buildUxAS_WithDocker.sh
@@ -15,12 +15,15 @@
 # 	  --mount type=bind,source="${PWD}/../../..",target="/UxASDev"  steveras/uxas-build:x86_64 
 # fi
 
+echo "##### DOCKER RUN #####"
 docker run -it -d \
   --name uxas_build  -w="/UxASDev/OpenUxAS"\
   --mount type=bind,source="${PWD}/../../..",target="/UxASDev"  steveras/uxas-build:x86_64 
 
 
+echo "##### RunLmcpGen #####"
 docker exec -it uxas_build  bash /UxASDev/OpenUxAS/RunLmcpGen.sh
+echo "##### buildUxAS #####"
 docker exec -it uxas_build  bash /UxASDev/OpenUxAS/docker/develop/buildUxAS.sh
 
 docker stop uxas_build
