476cd2533cbab99f16b50e5128ab4ba0b3bdfad1
steve <steve@steve-16.04>
2018-01-03 12:59:26 -0500

    added scripts to run UxAS tests in docker container

---
+++ docker/develop/04_runUxAS_Tests.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+#!/bin/bash -e
+
+# if [ ! "$(docker ps -q -f name=uxas_build)" ]; then
+#     if [ "$(docker ps -aq -f status=exited -f name=uxas_build)" ]; then
+#         docker rm uxas_build
+#     fi
+#     # run your container
+# 	docker run \
+# 	  -it \
+# 	  -d \
+# 	  --name uxas_build  -w="/UxASDev/OpenUxAS"\
+# 	  --mount type=bind,source="${PWD}/../../..",target="/UxASDev"  steveras/uxas-build:x86_64 
+# fi
+
+docker run -it -d \
+  --name uxas_build  -w="/UxASDev/OpenUxAS"\
+  --mount type=bind,source="${PWD}/../../..",target="/UxASDev"  steveras/uxas-build:x86_64 
+
+
+docker exec -it uxas_build  bash /UxASDev/OpenUxAS/docker/develop/runUxAS_tests.sh
+
+docker stop uxas_build
+docker rm uxas_build
