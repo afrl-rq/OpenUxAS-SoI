0d1d35d409d0b7f9e180cd974492bf474e2ff128
steve <steve@steve-16.04>
2018-01-03 16:40:06 -0500

    - fixed docker to work with internal code, updated readme's

--- docker/develop/ReadMe.md
+++ docker/develop/ReadMe.md
@@ -2,6 +2,21 @@ The files in this directory are used to construct
 and deploy a Docker container that is used to build
 UxAS.
 
+Note: at this time the image "steveras/uxas-build:x86_64"
+	is available on th Docker Hub, "https://hub.docker.com/", 
+	which Docker can download automatically, skipping step #1 
+	(01_buildImage_UxAS_build.sh). 
+
+
+BEFORE BUILDING UxAS:
+1) Must have the following file system layout:
+./
+  OpenAMASE (OPTIONAL)
+  LcmpGen
+  OpenUxAS
+
+
+
 FILES:
 
 01_buildImage_UxAS_build.sh - this is a linux script that 
--- docker/develop/buildUxAS.sh
+++ docker/develop/buildUxAS.sh
@@ -1,12 +1,27 @@
 #!/bin/bash -e
 
+IS_INTERNAL=$1
+USE_INTERNAL="false"
+if [ "$IS_INTERNAL" = "true" ]
+then
+	USE_INTERNAL="true"
+fi
+
 # 1 - change to the directory: OpenUxAS
 cd /UxASDev/OpenUxAS
 
-# if builds exists the just run Ninja
-if [ ! -d "build" ]; then
-	meson build --buildtype=release
+# 2
+# if "build" exists the just run Ninja
+if [ ! -d "build" ]; then 
+	echo "##### NEW MESON BUILD DIRECTORY #####"
+	meson build  -Dafrl_internal=${USE_INTERNAL} -Dinternal_3rd_libraries=false --buildtype=release
+else
+	echo "##### USING EXISTING MESON BUILD DIRECTORY #####"
+	cd build
+	meson configure -Dafrl_internal=${USE_INTERNAL} -Dinternal_3rd_libraries=false
+	cd ..
 fi
+
 
 # 3 - compile the code
 ninja -C build all
