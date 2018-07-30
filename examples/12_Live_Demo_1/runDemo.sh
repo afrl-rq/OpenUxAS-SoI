7698868a51f4cc9767c273a4f69501ad9c0f0f1a
Paul Coen <pcoen142@gmail.com>
2018-07-30 13:41:26 -0400

    Changed the demo scripts to work on both of our machines

--- examples/12_Live_Demo_1/runDemo.sh
+++ examples/12_Live_Demo_1/runDemo.sh
@@ -4,28 +4,49 @@
 
 TEMPVAR=$(pwd)
 
+#This is to remove the 
 cd /dev/mqueue
 rm -r ./*
 cd $TEMPVAR
 
-cd $HOME/UxAS_pulls/OpenUxAS/examples/12_Live_Demo_1/
+#Demo number goes here [1-3]
+DEMO="1"
 
+if [ $USER = "nia" ]
+then
+    #This is for Winston's directory structure
+    cd $HOME/uxas-pulls/OpenUxAS/examples/12_Live_Demo_${DEMO}/
 
+    ./runAMASE_Live_Demo_${DEMO}.sh&
 
-./runAMASE_Live_Demo_1.sh&
+    sleep 3
+    gnome-terminal --geometry=74x19+-10+14 --command "sh -c 'echo; cd ${HOME}/uxas-pulls/OpenUxAS/examples/12_Live_Demo_${DEMO}/; ./runUxAS_Live_Demo_${DEMO}.sh; exec bash'"
 
-sleep 3
+    sleep 2
+    gnome-terminal --geometry=74x19--5+14 --command "sh -c 'echo; cd ${HOME}/icarous-pulls/icarous/cFS/bin/cpu1/ && ./core-cpu1; exec bash'"
 
-gnome-terminal --geometry=74x19+-10+14 --command "sh -c 'echo; cd ${HOME}/UxAS_pulls/OpenUxAS/examples/12_Live_Demo_1/; ./runUxAS_Live_Demo_1.sh; exec bash'"
+    sleep 4
+    gnome-terminal --geometry=74x19--5-29 --command "sh -c 'echo; cd ${HOME}/icarous-pulls/icarous/cFS/bin/cpu1/ && ./core-cpu1; exec bash'"
 
-sleep 2
+    sleep 4
+    gnome-terminal --geometry=74x19+-10-29 --command "sh -c 'echo; cd ${HOME}/icarous-pulls/icarous/cFS/bin/cpu1/ && ./core-cpu1; exec bash'"
+    #End of Winston's
+else
+    #This is for Paul's directory structure
+    cd $HOME/UxAS_pulls/OpenUxAS/examples/12_Live_Demo_${DEMO}/
 
-gnome-terminal --geometry=74x19--5+14 --command "sh -c 'echo; cd ${HOME}/ICAROUS_pulls/icarous/cFS/bin/cpu1/ && ./core-cpu1; exec bash'"
+    ./runAMASE_Live_Demo_${DEMO}.sh&
 
-sleep 4
+    sleep 3
+    gnome-terminal --geometry=74x19+-10+14 --command "sh -c 'echo; cd ${HOME}/UxAS_pulls/OpenUxAS/examples/12_Live_Demo_${DEMO}/; ./runUxAS_Live_Demo_${DEMO}.sh; exec bash'"
 
-gnome-terminal --geometry=74x19--5-29 --command "sh -c 'echo; cd ${HOME}/ICAROUS_pulls/icarous/cFS/bin/cpu1/ && ./core-cpu1; exec bash'"
+    sleep 2
+    gnome-terminal --geometry=74x19--5+14 --command "sh -c 'echo; cd ${HOME}/ICAROUS_pulls/icarous/cFS/bin/cpu1/ && ./core-cpu1; exec bash'"
 
-sleep 4
+    sleep 4
+    gnome-terminal --geometry=74x19--5-29 --command "sh -c 'echo; cd ${HOME}/ICAROUS_pulls/icarous/cFS/bin/cpu1/ && ./core-cpu1; exec bash'"
 
-gnome-terminal --geometry=74x19+-10-29 --command "sh -c 'echo; cd ${HOME}/ICAROUS_pulls/icarous/cFS/bin/cpu1/ && ./core-cpu1; exec bash'"
+    sleep 4
+    gnome-terminal --geometry=74x19+-10-29 --command "sh -c 'echo; cd ${HOME}/ICAROUS_pulls/icarous/cFS/bin/cpu1/ && ./core-cpu1; exec bash'"
+    #End of Paul's
+fi
--- examples/12_Live_Demo_2/runDemo.sh
+++ examples/12_Live_Demo_2/runDemo.sh
@@ -4,24 +4,43 @@
 
 TEMPVAR=$(pwd)
 
+#This is to remove the 
 cd /dev/mqueue
 rm -r ./*
 cd $TEMPVAR
 
-cd $HOME/UxAS_pulls/OpenUxAS/examples/12_Live_Demo_2/
+#Demo number goes here [1-3]
+DEMO="2"
 
+if [ $USER = "nia" ]
+then
+    #This is for Winston's directory structure
+    cd $HOME/uxas-pulls/OpenUxAS/examples/12_Live_Demo_${DEMO}/
 
+    ./runAMASE_Live_Demo_${DEMO}.sh&
 
-./runAMASE_Live_Demo_2.sh&
+    sleep 3
+    gnome-terminal --geometry=74x19+-10+14 --command "sh -c 'echo; cd ${HOME}/uxas-pulls/OpenUxAS/examples/12_Live_Demo_${DEMO}/; ./runUxAS_Live_Demo_${DEMO}.sh; exec bash'"
 
-sleep 3
+    sleep 2
+    gnome-terminal --geometry=74x19--5+14 --command "sh -c 'echo; cd ${HOME}/icarous-pulls/icarous/cFS/bin/cpu1/ && ./core-cpu1; exec bash'"
 
-gnome-terminal --geometry=74x19+-10+14 --command "sh -c 'echo; cd ${HOME}/UxAS_pulls/OpenUxAS/examples/12_Live_Demo_2/; ./runUxAS_Live_Demo_2.sh; exec bash'"
+    sleep 4
+    gnome-terminal --geometry=74x19--5-29 --command "sh -c 'echo; cd ${HOME}/icarous-pulls/icarous/cFS/bin/cpu1/ && ./core-cpu1; exec bash'"
+    #End of Winston's
+else
+    #This is for Paul's directory structure
+    cd $HOME/UxAS_pulls/OpenUxAS/examples/12_Live_Demo_${DEMO}/
 
-sleep 2
+    ./runAMASE_Live_Demo_${DEMO}.sh&
 
-gnome-terminal --geometry=74x19--5+14 --command "sh -c 'echo; cd ${HOME}/ICAROUS_pulls/icarous/cFS/bin/cpu1/ && ./core-cpu1; exec bash'"
+    sleep 3
+    gnome-terminal --geometry=74x19+-10+14 --command "sh -c 'echo; cd ${HOME}/UxAS_pulls/OpenUxAS/examples/12_Live_Demo_${DEMO}/; ./runUxAS_Live_Demo_${DEMO}.sh; exec bash'"
 
-sleep 4
+    sleep 2
+    gnome-terminal --geometry=74x19--5+14 --command "sh -c 'echo; cd ${HOME}/ICAROUS_pulls/icarous/cFS/bin/cpu1/ && ./core-cpu1; exec bash'"
 
-gnome-terminal --geometry=74x19--5-29 --command "sh -c 'echo; cd ${HOME}/ICAROUS_pulls/icarous/cFS/bin/cpu1/ && ./core-cpu1; exec bash'"
+    sleep 4
+    gnome-terminal --geometry=74x19--5-29 --command "sh -c 'echo; cd ${HOME}/ICAROUS_pulls/icarous/cFS/bin/cpu1/ && ./core-cpu1; exec bash'"
+    #End of Paul's
+fi
