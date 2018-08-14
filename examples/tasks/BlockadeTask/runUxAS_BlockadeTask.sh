d5fc140d78b4b25d9f74d002a34b764ec964b958
Jon <steingass.5@gmail.com>
2018-08-14 12:30:47 -0400

    added blockade task example

---
+++ examples/tasks/BlockadeTask/runUxAS_BlockadeTask.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+SAVE_DIR=$(pwd)
+
+RM_DATAWORK="rm -R ./datawork"
+RM_LOG="rm -R ./log"
+
+BIN="../../../../build/uxas"
+
+mkdir -p RUNDIR_BlockadeTask
+cd RUNDIR_BlockadeTask
+$RM_DATAWORK
+$RM_LOG
+$BIN -cfgPath ../cfg_BlockadeTask.xml
+
