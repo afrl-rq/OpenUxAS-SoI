c0d3d1f25c2530a2c073e1e5cc58f7c7b51c2c12
Jon <steingass.5@gmail.com>
2018-08-17 14:46:33 -0400

    replaced startup scripts with python scripts

--- examples/tasks/LoiterTask/runAMASE_LoiterTask.sh
+++
@@ -1,6 +0,0 @@
-here=$PWD;
-
-cd ../../../../OpenAMASE/OpenAMASE;
-java -Xmx2048m -splash:./data/amase_splash.png -classpath ./dist/*:./lib/*  avtas.app.Application --config config/amase --scenario "../../OpenUxAS/examples/tasks/LoiterTask/Scenario_LoiterTask.xml";
-cd "$here";
-
