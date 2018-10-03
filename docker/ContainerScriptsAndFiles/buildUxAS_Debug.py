b3d794893da5001d9314c84afa0dad4420f5fb67
unknown <steingassj.adm@HP-5CB3350GVW.wsri.local>
2018-10-03 14:46:09 -0400

    modified 08_Build_UxAS_Debug.py to run python scipts

---
+++ docker/ContainerScriptsAndFiles/buildUxAS_Debug.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python3
+import time
+import sys
+import os
+from subprocess import call 
+
+def callWithShell(cmd):
+    call(cmd,shell=True)
+
+
+startAllTime = time.time()
+
+
+print("\n*** Sync Files ### \n")
+sys.stdout.flush()
+callWithShell("python /UxASDev/OpenUxAS/docker/ContainerScriptsAndFiles/syncUxASFiles.py")
+
+print( "\n#### RUNNING MESON ####\n")
+sys.stdout.flush()
+
+startMeson = time.time()
+
+# 1 - change to the directory: OpenUxAS
+callWithShell("cd /tmp_build")
+
+# 2
+# if "build" exists the just run Ninja
+if(os.path.isdir("build_debug")):
+    callWithShell("meson build_debug --buildtype=debug")
+
+print( "\n#### FINISHED RUNNING MESON [{}] ####\n.".format(time.time() - startMeson))
+sys.stdout.flush()
+
+print("\n#### RUNNING NINJA ####\n")
+sys.stdout.flush()
+
+ninjaStart = time.time()
+
+# 3 - compile the code
+
+callWithShell("ninja -C build_debug all")
+
+print("\n#### FINISHED RUNNING NINJA [{}] ####\n".format(time.time() - ninjaStart))
+
+callWithShell("mkdir -p /UxASDev/OpenUxAS/docker/tmp/debug")
+
+callWithShell("cp /tmp_build/build_debug/uxas /UxASDev/OpenUxAS/docker/tmp/debug/uxas")
+
+print( "\n#### FINISHED! Total Time [{}] ####\n".format(time.time() - startAllTime))
+
