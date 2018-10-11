711878f4c73578bfaf46aa39ba90ddcf08acb048
unknown <steingassj.adm@HP-5CB3350GVW.wsri.local>
2018-10-11 11:49:24 -0400

    travis ci looks at correct directory

--- ci/travis_docker.sh
+++ ci/travis_docker.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/afrl-rq/LmcpGen.git ../LmcpGen
 
-(pushd docker/develop; python3 ./01_construct_uxas-build.py && python3  ./02_PrepareAndBuildLmcp.py && python3  ./03_BuildDeploy_UxAS.py && python3  ./05_Run_UxAS-Tests.py; popd)
+(pushd docker; python3 ./01_construct_uxas-build.py && python3  ./02_PrepareAndBuildLmcp.py && python3  ./03_BuildDeploy_UxAS.py && python3  ./05_Run_UxAS-Tests.py; popd)
