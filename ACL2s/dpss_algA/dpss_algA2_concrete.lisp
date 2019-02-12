d2e9d3d98d04c296ed4acb0504392db6c5ea31e6
wallinjosh <jwallin@iastate.edu>
2019-02-12 14:04:53 -0600

    updating models

--- ACL2s/dpss_algA/dpss_algA2_concrete.lisp
+++ ACL2s/dpss_algA/dpss_algA2_concrete.lisp
@@ -1,32 +1,45 @@
-; ****************** BEGIN INITIALIZATION FOR ACL2s MODE ****************** ;
+; *************** BEGIN INITIALIZATION FOR PROGRAMMING MODE *************** ;
 ; (Nothing to see here!  Your actual file is after this initialization code);
 
-#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading the CCG book.~%") (value :invisible))
-(include-book "acl2s/ccg/ccg" :uncertified-okp nil :dir :system :ttags ((:ccg)) :load-compiled-file nil);v4.0 change
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading the TRACE* book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+; only load for interactive sessions: 
+#+acl2s-startup (include-book "trace-star" :uncertified-okp nil :dir :acl2s-modes :ttags ((:acl2s-interaction)) :load-compiled-file nil);v4.0 change
 
-;Common base theory for all modes.
-#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s base theory book.~%") (value :invisible))
-(include-book "acl2s/base-theory" :dir :system :ttags :all)
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading the EVALABLE-LD-PRINTING book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+; only load for interactive sessions: 
+#+acl2s-startup (include-book "hacking/evalable-ld-printing" :uncertified-okp nil :dir :system :ttags ((:evalable-ld-printing)) :load-compiled-file nil);v4.0 change
 
 
-#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s customizations book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
-(include-book "custom" :dir :acl2s-modes :ttags :all)
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s customizations book.~%") (value :invisible))
+(include-book "acl2s/defunc" :dir :system :uncertified-okp nil :load-compiled-file :comp) ;lets add defunc at least harshrc [2015-02-01 Sun]
+(include-book "custom" :dir :acl2s-modes :uncertified-okp nil :load-compiled-file :comp)
 
-#+acl2s-startup (er-progn (assign fmt-error-msg "Problem setting up ACL2s mode.") (value :invisible))
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading programming mode.") (value :invisible))
 
-;Settings common to all ACL2s modes
-(acl2s-common-settings)
-;(acl2::xdoc acl2s::defunc) ;; 3 seconds is too much time to spare -- commenting out [2015-02-01 Sun]
 
-(acl2::xdoc acl2s::defunc) ; almost 3 seconds
-
-; Non-events:
-;(set-guard-checking :none)
-
+(er-progn 
+  (program)
+  (defun book-beginning () ()) ; to prevent book development
+  (set-irrelevant-formals-ok :warn)
+  (set-bogus-mutual-recursion-ok :warn)
+  (set-ignore-ok :warn)
+  (set-verify-guards-eagerness 0)
+  (set-default-hints '(("Goal" :error "This depends on a proof, and proofs are disabled in Programming mode.  The session mode can be changed under the \"ACL2s\" menu.")))
+  (reset-prehistory t)
+  (set-guard-checking :none)
+  (set-guard-checking :nowarn)
+  (assign evalable-ld-printingp t)
+  (assign evalable-printing-abstractions '(list cons))
+  (assign triple-print-prefix "; "))
+  
 (acl2::in-package "ACL2S")
 
-; ******************* END INITIALIZATION FOR ACL2s MODE ******************* ;
-;$ACL2s-SMode$;ACL2s
+(cw "~@0Programming mode loaded.~%~@1"
+      #+acl2s-startup "${NoMoReSnIp}$~%" #-acl2s-startup ""
+      #+acl2s-startup "${SnIpMeHeRe}$~%" #-acl2s-startup "")
+
+; **************** END INITIALIZATION FOR PROGRAMMING MODE **************** ;
+;$ACL2s-SMode$;Programming
 ;DPSS CONSTANTS
 (defconst *n_int* 3)
 (defconst *n_real* 3.)
@@ -46,59 +59,73 @@
 (defdata direction (enum '(left right)))
 (defdata position (range rational (0. <= _ <= *p*)))
 (defdata id (range integer (0. < _ <= *n_int*)))
-(defdata state (enum '(null escorted escorting converged)))
+;(defdata state (enum '(null escorted escorting converged)))
 
 
 ;Record (struct) representing single UAV
 (defdata UAS (record (uasid . id)
                      (dir . bool)
+                     (pre_dir . bool)
                      (loc . position)
+                     (pre_loc . position)
                      (goal . position)
-                     (st . state)
+                     (pre_goal . position)
+                     ;(st . state)
                      (meet_ln . bool)
                      (meet_rn . bool)
                      (s_l . position)
                      (s_r . position)))
 
 ;Full system consisting of a list of UAVs
-(defdata system (listof UAS))
+;(defdata system (listof UAS))
 
-;Direction update function
+;Direction update
 (defun set_direction (ag)
   (if (uasp ag)
-    (if (<= (uas-loc ag) 0.)
+    (if (<= (uas-pre_loc ag) 0.)
       (set-uas-dir *right* ag)
-      (if (>= (uas-loc ag) *p*)
+      (if (>= (uas-pre_loc ag) *p*)
         (set-uas-dir *left* ag)
         (if (= (uas-meet_ln ag) *true_int*)
-          (if (<= (uas-loc ag) (uas-loc ag))
+          (if (<= (uas-pre_loc ag) (uas-pre_loc ag))
             (set-uas-dir *right* ag)
             (set-uas-dir *left* ag))
           (if (= (uas-meet_rn ag) *true_int*)
-            (if (<= (uas-loc ag) (uas-s_r ag))
+            (if (<= (uas-pre_loc ag) (uas-s_r ag))
               (set-uas-dir *right* ag)
               (set-uas-dir *left* ag))
             ()))))()))
 
-;Goal update function
+;Goal update
 (defun set_goal (ag)
   (if (uasp ag)
     (if (and (= (uas-meet_ln ag) 1) (= (uas-meet_rn ag) 1))
-      (if (<= (uas-loc ag) (uas-s_l ag))
+      (if (<= (uas-pre_loc ag) (uas-s_l ag))
         (set-uas-goal (uas-s_r ag) ag)
         (set-uas-goal (uas-s_l ag) ag))
       (if (= (uas-meet_ln ag) 1)
-        (if (<= (uas-loc ag) (uas-s_l ag))
+        (if (<= (uas-pre_loc ag) (uas-s_l ag))
           (set-uas-goal *p* ag)
           (set-uas-goal (uas-s_l ag) ag))
         (if (= (uas-meet_rn ag) 1)
-          (if (>= (uas-loc ag) (uas-s_r ag))
+          (if (>= (uas-pre_loc ag) (uas-s_r ag))
             (set-uas-goal 0. ag)
             (set-uas-goal (uas-s_r ag) ag))
-          (if (= (uas-dir ag) *right*)
+          (if (= (uas-pre_dir ag) *right*)
             (set-uas-goal *p* ag)
             (set-uas-goal 0. ag)))))
     ())
+  )
+
+;Position update
+(defun set_loc (ag dt)
+  (if (and (uasp ag) (rationalp dt))
+    (if (= (uas-dir ag) *right*)
+      (set-uas-loc (+ (uas-loc ag) (* *v* dt)) ag)
+      (set-uas-loc (- (uas-loc ag) (* *v* dt)) ag)
+      )
+    ()
+    )
   )
 
 ;Getters for record entries
@@ -162,30 +189,104 @@
 (defun delta_t_nums (uas_list)
   (if (listp uas_list)
     (if (= (len uas_list) 0)
-      '(100) ;If this is an empty list, return a high number that is likely to get beaten out by the min call
+      '(100.) ;If this is an empty list, return a high number that is likely to get beaten out by the min call
         (min_time (nth 1 uas_list) (delta_t_nums (nthcdr 1 uas_list))))
   '(1.)))
 
 (defmacro p_12 () (* 1. (/ *p* *n_real*)))
 (defmacro p_23 () (* 2. (/ *p* *n_real*)))
 
+(defun optimal? (uas1 uas2 uas3)
+  (and (and (uasp uas1) (uasp uas2) (uasp uas3))
+    (or (and (and (= (uas-loc uas1) 0.) (= (uas-loc uas2) (p_23)) (= (uas-loc uas3) (p_23)))
+             (and (= (uas-pre_loc uas1) (p_12)) (= (uas-loc uas2) (p_12)) (= (uas-loc uas3) *p*)))
+        (and (and (= (uas-loc uas1) (p_12)) (= (uas-loc uas2) (p_12)) (= (uas-loc uas3) *p*))
+             (and (= (uas-pre_loc uas1) 0.) (= (uas-loc uas2) (p_23)) (= (uas-loc uas3) (p_23)))))
+    )
+  )
+
+;TODO: how do we update state for the UAS during these recursive calls?
+
 ;Recursively evaluates next steps for uavs
 (defun DPSS_eval (uas1 uas2 uas3)
-  (if (and (uasp uas1) (uasp uas2) (uasp uas3))
-    (mget :goal uas1)
-    ()))
+    (if (and (uasp uas1) (uasp uas2) (uasp uas3))
+      (let ((dt (min_time (time_to_reach_target_position (uas-dir uas1) (uas-loc uas1) (uas-goal uas1))
+                     (min_time (time_to_reach_target_position (uas-dir uas2) (uas-loc uas2) (uas-goal uas2))
+                               (min_time (time_to_reach_target_position (uas-dir uas3) (uas-loc uas3) (uas-goal uas3))
+                                         (min_time (time_to_reach_neighbor (uas-dir uas1) (uas-dir uas2) (uas-loc uas1) (uas-loc uas2))
+                                                   (time_to_reach_neighbor (uas-dir uas2) (uas-dir uas3) (uas-loc uas2) (uas-loc uas3))))))))
+      (if (optimal? uas1 uas2 uas3)
+        '0;termination of recursion: reached optimal state so 'no more important events';recursion: add time to next step ('important event') to recursive call to evaluate
+      (progn$
+       ;UPDATE STATE HERE
+       ;Update previous positions
+       (set-uas-pre_loc (uas-loc uas1) uas1)
+       (set-uas-pre_loc (uas-loc uas2) uas2)
+       (set-uas-pre_loc (uas-loc uas3) uas3)
+       ;Update previous directions
+       (set-uas-pre_dir (uas-dir uas1) uas1)
+       (set-uas-pre_dir (uas-dir uas2) uas2)
+       (set-uas-pre_dir (uas-dir uas3) uas3)
+       ;Update previous goals
+       (set-uas-pre_goal (uas-goal uas1) uas1)
+       (set-uas-pre_goal (uas-goal uas2) uas2)
+       (set-uas-pre_goal (uas-goal uas3) uas3)
+       ;Use the dt we calculated before to "step" all of the UAVs forward
+       
+       ;Update meet variables if necessary
+       (if (= (uas-loc uas1) (uas-loc uas2))
+         (progn$
+          (set-uas-meet_rn 't uas1)
+          (set-uas-meet_ln 't uas2))
+         ())
+       (if (= (uas-loc uas2) (uas-loc uas3))
+         (progn$
+          (set-uas-meet_rn 't uas2)
+          (set-uas-meet_ln 't uas3))
+         ())
+       
+       ;Step forward UAS1
+       (set_direction uas1)
+       (set_goal uas1)
+       (set_loc uas1 dt)
+       
+       ;Step forward UAS2
+       (set_direction uas2)
+       (set_goal uas2)
+       (set_loc uas2 dt)
+       
+       ;Step forward UAS3
+       (set_direction uas3)
+       (set_goal uas3)
+       (set_loc uas3 dt)
+       
+       ;Print out the updated state information
+       (cw "stepping")
+       
+        (+ dt
+           (DPSS_eval uas1 uas2 uas3)
+           );evaluate steps
+        )
+      ))
+      '0;if objects passed in aren't uas, evaluate to zero; vacuously satisfies any properties
+      )
+  )
 
 ;Initializes uavs using given initial positions
 (defun DPSS (init_pos_uas1 init_pos_uas2 init_pos_uas3 init_dir_uas1 init_dir_uas2 init_dir_uas3)
   (let*  ((uas1 (nth-uas 1))
          (uas2 (nth-uas 1))
          (uas3 (nth-uas 1)))
-    (and (set-uas-loc init_pos_uas1 uas1)
+    (and 
+     (set-uas-loc init_pos_uas1 uas1)
     (set-uas-loc init_pos_uas2 uas2)
     (set-uas-loc init_pos_uas3 uas3)
-    (set-uas-loc init_dir_uas1 uas1)
-    (set-uas-loc init_dir_uas2 uas2)
-    (set-uas-loc init_dir_uas3 uas3)
+    (set-uas-pre_loc init_pos_uas1 uas1)
+    (set-uas-pre_loc init_pos_uas2 uas2)
+    (set-uas-pre_loc init_pos_uas3 uas3)
+    (set-uas-dir init_dir_uas1 uas1)
+    (set-uas-dir init_dir_uas2 uas2)
+    (set-uas-dir init_dir_uas3 uas3)
     (set-uas-st nil uas1)
     (set-uas-st nil uas2)
     (set-uas-st nil uas3)
@@ -214,6 +315,7 @@
    )
 )
 
-;CONCRETE PROOF (uses n to create concrete number of UAVs)
-(defmacro time_to_reach_neighbor_12 () )
-
+(defthm uas_time_bound
+  (implies (and (positionp p_uas1) (positionp p_uas2) (positionp p_uas3) (directionp d_uas1) (directionp d_uas2) (directionp d_uas3))
+           (<= (DPSS p_uas1 p_uas2 p_uas3 d_uas1 d_uas2 d_uas3)
+              (* 2 (dpss_t)))))
