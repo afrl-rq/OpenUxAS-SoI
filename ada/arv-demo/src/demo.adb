59b72e4733a300eab8f1779f68e93fc1209f6599
Joffrey Huguet <huguet@adacore.com>
2019-11-08 10:58:54 +0100

    Add gcov qualifier and support to the Ada demo

--- ada/arv-demo/src/demo.adb
+++ ada/arv-demo/src/demo.adb
@@ -1,5 +1,7 @@
 with Ada.Text_IO;    use Ada.Text_IO;
 with Ada.Exceptions; use Ada.Exceptions;
+with GNAT.OS_Lib;
+with GNAT.Ctrl_C;
 
 with UxAS.Common.Configuration_Manager;  use UxAS.Common;
 
@@ -19,6 +21,21 @@ procedure Demo is
 
    All_Enabled_Services : DOM.Core.Element;
 begin
+
+   #if GCOV then
+   declare
+      procedure Flush;
+      pragma Import (C, Flush, "__gcov_flush");
+      procedure SIGINT_Exit is
+      begin
+         Flush;
+         GNAT.OS_Lib.OS_Exit (1);
+      end SIGINT_Exit;
+   begin
+      GNAT.Ctrl_C.Install_Handler (SIGINT_Exit'Unrestricted_Access);
+   end;
+   #end if;
+
    Configuration_Manager.Instance.Load_Base_XML_File (XML_Cfg_File_Name, Success);
    if not Success then
       Put_Line ("Could not load base XML file '" & XML_Cfg_File_Name & "'");
