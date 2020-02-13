7310e66348eaeefd43024257ae9eef7aae7b2f94
rogers <rogers@adacore.com>
2020-02-13 17:51:02 -0600

    minor refinement, to more easily detect null TCP addresses

--- ada/arv-demo/src/demo.adb
+++ ada/arv-demo/src/demo.adb
@@ -110,8 +110,8 @@ procedure Demo is
             if Service_Type_Name = "LmcpObjectNetworkPublishPullBridge" then
                Pub_Address  := To_Unbounded_String (DOM.Core.Elements.Get_Attribute (Next, Name => "AddressPUB"));
                Pull_Address := To_Unbounded_String (DOM.Core.Elements.Get_Attribute (Next, Name => "AddressPULL"));
-               Put_Line ("Found bridge addressPUB:  " & To_String (Pub_Address));
-               Put_Line ("Found bridge addressPULL: " & To_String (Pull_Address));
+               Put_Line ("Found bridge addressPUB:  """ & To_String (Pub_Address) & '"');
+               Put_Line ("Found bridge addressPULL: """ & To_String (Pull_Address) & '"');
                return;
             end if;
             Next := Next_Sibling (Next);
